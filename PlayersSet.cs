7e11bc5d181374de59672e548317259539f86c04
Дилян Йовчев <didiyovchev0@gmail.com>
2023-07-06 13:57:44 +0300

    1

--- PlayersSet.cs
+++ PlayersSet.cs
@@ -3,50 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace TriviaTrek
 {
     class PlayersSet
     {
         public int[] playerLocation;
-        int[] players;
-        int spaces = 51;
-        int currentPlayer = 0;
-        public int PlayerAmount;
+        public string[] players;
+        public int spaces = 51;
+        public int currentPlayer = 0;
 
-        public PlayersSet(int PlayerAmount) 
-        { 
-            this.PlayerAmount = PlayerAmount;
+        static StreamReader sr = new StreamReader("C:\\Users\\User\\source\\repos\\TriviaTrek\\Questions.txt");
+        string[] question = sr.ReadLine().Split(',').Reverse().ToArray();
+        
+        public PlayersSet() 
+        {
         }
         public void Setup() 
-        { 
+        {
+            Console.WriteLine("Mоля въведете число от 2 до 4.");
+            int playerAmount = int.Parse(Console.ReadLine());
             while (true)
             {
-                if (PlayerAmount > 1 && PlayerAmount < 5)
+                if (playerAmount > 1 && playerAmount < 5)
                 {
-                    players = new int[PlayerAmount];
-                    for (int i = 1; i < PlayerAmount; i++)
+                    players = new string[playerAmount];
+                    for (int i = 0; i < playerAmount; i++)
                     {
-                        players[i]++;
+                        Console.WriteLine($"Моля въведете име на играч номер {i+1}");
+                        players[i] = Console.ReadLine();
                     }
-                    playerLocation = new int[PlayerAmount];
+                    playerLocation = new int[playerAmount];
                     break;
                 }
-                else if (PlayerAmount < 2)
+                else if (playerAmount < 2)
                 {
                     Console.WriteLine("Твърде малко играчи моля въведете число от 2 до 4.");
-                    PlayerAmount = int.Parse(Console.ReadLine());
+                    playerAmount = int.Parse(Console.ReadLine());
                 }
                 else
                 {
                     Console.WriteLine("Твърде много играчи моля въведете число от 2 до 4.");
-                    PlayerAmount = int.Parse(Console.ReadLine());
-                }
+                    playerAmount = int.Parse(Console.ReadLine());
+                }   
            
             }
             
         }
-        
-
+        public void Move()
+        {
+            List<string> questions = new List<string> { question[2] };
+            Console.WriteLine(questions[0]);
+            Random random = new Random();
+            int roll = random.Next(1,6);
+            Console.WriteLine($"Падна ти се {roll}");
+            playerLocation[currentPlayer] += roll;
+            for (int i = 0; i < playerLocation.Length; i++)
+            {
+                Console.Write(playerLocation[i] + " ");
+                for (int j = 0; j < playerLocation[i]; j++)
+                {
+                    Console.Write("-");
+                }
+                Console.WriteLine(" " + players[i]);
+                
+            }
+            
+            if (roll == 1 || roll == 2)
+            {
+                
+                
+            }
+        }
+        public void showPlayer()
+        {
+            Console.WriteLine(players[currentPlayer]);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TriviaTrek
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Mоля въведете число от 2 до 4.");
-            int playerAmount = int.Parse(Console.ReadLine());
-            PlayersSet set1 = new PlayersSet(playerAmount);
-            set1.Setup();
-        }
+            try
+            {
+                Console.WriteLine("===Здравей моля избери една от опциите:===\n1) Започни нова игра\n2) Изход");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        PlayersSet game = new PlayersSet();
+                        game.Setup();
+                        while (true)
+                        {
+                            Console.WriteLine("===Моля избери една от опциите:=== \n 1) Хвърли зарчето \n 2) Виж кой е на ред \n 3) Започни нова игра \n 4) Изход");
+                            choice = int.Parse(Console.ReadLine());
+                            switch (choice)
+                            {
+                                case 1:
+                                    game.Move(); break;
+                                case 2:
+                                    game.showPlayer(); break;
+                                case 3:
+                                    string file = Assembly.GetExecutingAssembly().Location;
+                                    System.Diagnostics.Process.Start(file); break;
+                                case 4: Environment.Exit(1); break;
+                            }
+                        }
+
+                    case 2: Environment.Exit(1); break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Въведохте невалидна команда! Стартиране на нова инстанция.");
+                string file = Assembly.GetExecutingAssembly().Location;
+                System.Diagnostics.Process.Start(file);
+            }
+            
+        }    
     }
 }
